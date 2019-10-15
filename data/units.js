700ce95f92a23d02be9e4881707f47719e6a19e5
Jonathan Moreno <jonathan.moreno324@gmail.com>
2019-10-15 13:54:19 -0500

    Show abbreviations in units dropdown
    - Clarified data/units.json as units.js
    - Created init() function for body onload

---
+++ data/units.js
@@ -0,0 +1,11 @@
+units =
+  {
+    "pounds": {
+      "abbreviation": "lbs",
+      "conversionFactor": "1"
+    },
+    "ounces": {
+      "abbreviation": "oz",
+      "conversionFactor": "16"
+    }
+  }
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,28 @@
-function testHeader () {
-    document.getElementById("main-header").innerHTML = "Hello JavaScript!";
-};
+var units = JSON.parse(units);
 
-function calculateUnitPrice () {
+function init() {
+    loadUnits();
+}
+
+function calculateUnitPrice() {
     var price = document.getElementById("price").value;
     var amount = document.getElementById("amount").value;
     var unit;
     var unitPrice;
-    unitPrice = price/amount;
-    
+    unitPrice = price / amount;
+
     document.getElementById("unitPrice").setAttribute("value", unitPrice);
+};
+
+function test() {
+    document.getElementById("testParagraph").append(units.pounds.abbreviation);
+};
+
+function loadUnits() {
+    for (const key in units) {
+        if (units.hasOwnProperty(key)) {
+            document.getElementById("unit").innerHTML += "<option value=" + key + ">" + units[key].abbreviation + "</option>"
+
+        }
+    }
 };
